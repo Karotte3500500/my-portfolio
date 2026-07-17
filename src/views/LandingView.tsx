7d68aa291e592bf415cf8f321f20ee41a9618423
Karotte3500500 <karotte250500@gmail.com>
2026-07-18 00:09:31 +0900

    Update meta tags in LandingView for improved SEO and social sharing

--- src/views/LandingView.tsx
+++ src/views/LandingView.tsx
@@ -17,7 +17,20 @@ function Landing(): React.JSX.Element {
 		<>
 			<Helmet>
 				<title>縄田 嵐 | Nawata's Lab</title>
-				<meta name="description" content="技術で社会課題と創作に挑戦する。C#とWebなどのIT技術を中心にものづくりをしています。"></meta>
+				<meta name="description" content="縄田嵐のポートフォリオサイト。高専生としてC#とWebなどのIT技術を中心にものづくりをしています。就活、インターン、共同開発のお問い合わせも歓迎です。"/>
+				<meta property="og:type" content="website" />
+  				<meta property="og:url" content="https://www.nawata.me/" />
+  				<meta property="og:site_name" content="Nawata's Lab" />
+
+				{/* あれ */}
+				<meta property="og:image" content="https://www.nawata.me/images/blog/created-portfolio-1/portfolio_sc.png" />
+  				<meta property="og:image:width" content="1200" />
+  				<meta property="og:image:height" content="630" />
+
+				<meta name="twitter:card" content="summary_large_image" />
+			  	<meta name="twitter:title" content="縄田 嵐 | Nawata's Lab" />
+  				<meta name="twitter:description" content="..." />
+  				<meta name="twitter:image" content="https://www.nawata.me/images/blog/created-portfolio-1/portfolio_sc.png" />
 			</Helmet>
 			
 			<section>
